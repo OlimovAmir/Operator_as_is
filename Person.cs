5ec853770039637376a7bb8e73be4bafb53190a6
olimov Amir <olimov.amir@mail.ru>
2023-12-31 09:14:08 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_as_is
+{
+    internal class Person
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Person(string name, string lastName, DateTime birthday)
+        {
+            Name = name;
+            LastName = lastName;
+            Birthday = birthday;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {LastName} {Birthday.ToShortDateString()}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Operator_as_is;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point point = new Point();
+
+        point.Print();
+    }
+
+
+
+    public void WriteToFile(List<Person> people, string filePath)
+    {
+        try
+        {
+            using(StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach(Person person in people)
+                {
+                    writer.WriteLine($"{person.Id} {person.Name} {person.LastName} {person.Birthday.ToShortDateString()}");
+                }
+                Console.WriteLine($"The data is written to a file {filePath}");
+            }
+        }catch (Exception ex)
+        {
+            Console.WriteLine($"error writing data {ex.Message}");
+        }
+    }
+}
